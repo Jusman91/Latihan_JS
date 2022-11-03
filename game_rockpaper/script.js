f783905acda8f8804f19e572d020c2528d50c44f
Jusman91 <jusmannine1@gmail.com>
2022-11-03 14:59:52 +0800

    add selector and objec

--- game_rockpaper/script.js
+++ game_rockpaper/script.js
@@ -1,7 +1,7 @@
 // menentukan pilihan komputer
 function choiceComp() {
     const comp = Math.random();
-    if(comp < 0.34) return 'gunting'
+    if(comp < 0.34) return 'gunting';
     if(comp >= 0.34 && comp < 0.67) return 'batu'; return 'kertas';
 }
 // menentukan hasil
@@ -12,15 +12,27 @@ function results(comp, player) {
     if(player == 'kertas') return (comp == 'gunting') ? 'YOU LOSE' : 'YOU WIN';
 }
 // memasukkan events
-const pGunting = document.querySelector('.gunting');
-pGunting.addEventListener('click', function() {
-    const cComp = choiceComp();
-    const cPlayer = pGunting.className;
-    const getResults = results(cComp, cPlayer);
+const pCanChoice = document.querySelectorAll('#player img');
+let scoreP = 0, scoreC = 0;
+pCanChoice.forEach(function(c) {
+    c.addEventListener('click', function() {
+        const cComp = choiceComp();
+        const cPlayer = c.className;
+        const getResults = results(cComp, cPlayer);
+        // mengacak pilihan komputer
+        const imgComp = document.getElementsByClassName('comp-choice')[0];
+        imgComp.setAttribute('src', `image/${cComp}.png`);
+        // menampilkan hasil
+        const finalResults = document.querySelector('.results');
+        finalResults.innerHTML = getResults;
+        // menentukan skor
+        if(getResults == 'YOU WIN') scoreP++;
+        else if(getResults == 'YOU LOSE') scoreC++;
+        // menampilkan skor
+        const compScore = document.querySelector('.comp-score');
+        const playerScore = document.querySelector('.player-score');
+        compScore.innerHTML = scoreC;
+        playerScore.innerHTML = scoreP;
 
-    const imgComp = document.getElementsByClassName('comp-choice')[0];
-    imgComp.setAttribute('src', `image/${cComp}.png`);
-
-    const finalResults = document.querySelector('.results');
-    finalResults.innerHTML = getResults;
+    })
 })
