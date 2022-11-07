3fee357e7a2741058376032a4ec8c1935fc52f8c
Jusman91 <jusmannine1@gmail.com>
2022-11-07 10:42:01 +0800

    add timing function

--- game_rockpaper/script.js
+++ game_rockpaper/script.js
@@ -11,6 +11,23 @@ function results(comp, player) {
     if(player == 'batu') return (comp == 'kertas') ? 'YOU LOSE' : 'YOU WIN';
     if(player == 'kertas') return (comp == 'gunting') ? 'YOU LOSE' : 'YOU WIN';
 }
+
+function roll() {
+    const imgComputer = document.querySelector('.comp-choice');
+    const picture = ['batu', 'gunting', 'kertas'];
+    let i = 0
+    const startTime = new Date().getTime();
+    setInterval(function() {
+        if(new Date().getTime() - startTime > 1000) {
+            clearInterval;
+            return;
+        }
+        imgComputer.setAttribute('src', 'image/' + picture[i++] + '.png')
+        if( i == picture.length) i = 0;
+    }, 100)
+}
+
+
 // memasukkan events
 const pCanChoice = document.querySelectorAll('#player img');
 let scoreP = 0, scoreC = 0;
@@ -19,20 +36,26 @@ pCanChoice.forEach(function(c) {
         const cComp = choiceComp();
         const cPlayer = c.className;
         const getResults = results(cComp, cPlayer);
-        // mengacak pilihan komputer
-        const imgComp = document.getElementsByClassName('comp-choice')[0];
-        imgComp.setAttribute('src', `image/${cComp}.png`);
-        // menampilkan hasil
-        const finalResults = document.querySelector('.results');
-        finalResults.innerHTML = getResults;
-        // menentukan skor
-        if(getResults == 'YOU WIN') scoreP++;
-        else if(getResults == 'YOU LOSE') scoreC++;
-        // menampilkan skor
-        const compScore = document.querySelector('.comp-score');
-        const playerScore = document.querySelector('.player-score');
-        compScore.innerHTML = scoreC;
-        playerScore.innerHTML = scoreP;
 
-    })
-})
+
+        roll();
+
+        setTimeout(function() {
+            // mengacak pilihan komputer
+            const imgComp = document.getElementsByClassName('comp-choice')[0];
+            imgComp.setAttribute('src', `image/${cComp}.png`);
+            // menampilkan hasil
+            const finalResults = document.querySelector('.results');
+            finalResults.innerHTML = getResults;
+            // menentukan skor
+            if(getResults == 'YOU WIN') scoreP++;
+            else if(getResults == 'YOU LOSE') scoreC++;
+            // menampilkan skor
+            const compScore = document.querySelector('.comp-score');
+            const playerScore = document.querySelector('.player-score');
+            compScore.innerHTML = scoreC;
+            playerScore.innerHTML = scoreP;
+        }, 1000);
+
+    });
+});
