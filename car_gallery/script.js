c1c199693a0d062eeefa9663bc0148539cd3d20c
Jusman91 <jusmannine1@gmail.com>
2022-11-08 08:46:50 +0800

    add style animation

--- car_gallery/script.js
+++ car_gallery/script.js
@@ -1,9 +1,12 @@
-const bigImage = document.querySelector('.big-img img');
-const miniImage = document.querySelector('.img1');
+const bigImage = document.querySelector('.big-img');
+const miniImage = document.querySelector('.mini-img');
 
 miniImage.addEventListener('click', function(e) {
     if(e.target.className == 'img1') {
-        e.target.parentElement.parentElement.previousElementSibling.setAttribute('src', 'image/audi.jpg')
+        bigImage.src = e.target.src;
+        bigImage.classList.add('fade');
+        setTimeout(function() {
+            bigImage.classList.remove('fade');
+        }, 500)
     }
 })
-console.log(miniImage.parentElement.parentElement.previousElementSibling)
